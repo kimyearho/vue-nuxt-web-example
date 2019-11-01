eb176dcd9cb767b471e7a4c82bf812b9d3fc9a1a
yeonho <yeonho@in-soft.co.kr>
2019-11-01 16:31:44 +0900

    .

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,6 +12,10 @@ export default {
   // middleware: "auth",
   components: {
     DashBoard
+  },
+  mounted() {
+    const token = localStorage.getItem('auth._token.google');
+    console.log(token)
   }
 };
 </script>
